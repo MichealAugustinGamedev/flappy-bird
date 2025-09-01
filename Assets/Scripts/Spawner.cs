77e1bfabe2dbabc8d688eebd35d387c006b6b9f6
MichealAugustinGamedev <michealaugustin0000@gmail.com>
2025-09-01 23:41:34 +0530

    Added Assets folder

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Pipes prefabs;
+    public float spwanRate = 1f;
+    public float minHeight = -1f;
+    public float maxHeight = 2f;
+    public float verticalGap = 3f;
+
+    private void OnEnable()
+    {
+        InvokeRepeating(nameof(Spawn), spwanRate, spwanRate);
+    }
+
+    private void OnDisable()
+    {
+        CancelInvoke(nameof(Spawner));
+    }
+
+    private void Spawn()
+    {
+        Pipes pipes = Instantiate(prefabs, transform.position, Quaternion.identity);
+        pipes.transform.position += Vector3.up * Random.Range(minHeight, maxHeight);
+        pipes.gap = verticalGap;
+    }
+}
